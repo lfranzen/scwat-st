a2ace2149af8631000c8d88a51865654563c7177
lfranzen <lovisa.franzen@scilifelab.se>
2021-01-22 15:24:58 +0100

    Adding new functions and permuted values
    Calculation of permuted values and comparison to observed values. Added new plots.

--- scripts/visium_nbs_analysis.R
+++ scripts/visium_nbs_analysis.R
@@ -56,7 +56,138 @@ if(ANALYSIS=="baseline"){
   c_include <- seq(4, max(as.numeric(se[[]]$seurat_clusters)), 1)
 }
 
-#' Run RegionNeighbours()
+
+#####
+#' Define functions
+CreateAdjMatrix <- function(
+  nbs.df,
+  cluster.include,
+  column.clusters.id = "seurat_clusters"
+  ){
+  nbs_adjmat <- matrix(0L, nrow = length(cluster.include), ncol = length(cluster.include))
+  for (i in seq_along(cluster.include)) {
+    c <- cluster.include[i]
+    c_nbs_df <- nbs.df[nbs.df[,column.clusters.id]==c, ]
+    for (j in seq_along(cluster.include)) {
+      nbs <- paste0("nbs_", cluster.include[j])
+      if (j==i) {
+        n_nbs <- sum(!is.na(c_nbs_df[, nbs]==c))
+      } else {
+        n_nbs <- sum(!is.na(c_nbs_df[, nbs]==nbs))
+      }
+      nbs_adjmat[i,j] <- n_nbs
+      if (nbs_adjmat[j,i] > 0) {
+        nbs_adjmat[i,j] <- nbs_adjmat[j,i] <- max(c(nbs_adjmat[i,j], nbs_adjmat[j,i]))
+      }
+    }
+    nbs_adjmat[i,i] <- sum(nbs_adjmat[i,][-i])
+  }
+  return(nbs_adjmat)
+}
+
+CreateAdjMatrixPerSubject <- function(
+  nbs.df,
+  se.metadata,
+  cluster.include,
+  column.clusters.id = "seurat_clusters",
+  column.subject.id
+){
+  nbs_adjmat_list <- list()
+  subjects_include <- unique(as.character(se.metadata[, column.subject.id]))
+  
+  for(subject_id in subjects_include){
+    rows_subject <- rownames(se.metadata[column.subject.id==subject_id, ])
+    nbs.df_subject <- nbs.df[rows_subject, ]
+    
+    nbs_adjmat <- CreateAdjMatrix(nbs.df = nbs.df_subject, 
+                                  cluster.include = cluster.include, 
+                                  column.clusters.id = column.clusters.id)
+    
+    nbs_adjmat_list[[subject_id]] <- nbs_adjmat
+  }
+  
+  return(nbs_adjmat_list)
+}
+
+minmax_norm <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+
+#####
+#' Expected values: RegionNeighbours() with permuted cluster IDs for each sample
+RandomiseClusteringIDs <- function (
+  se.object,
+  column.cluster.id,
+  column.sample.id,
+  random.seed = NA
+  ) {
+  if(!is.na(random.seed)){
+    message(paste("Setting random seed to", random.seed))
+    set.seed(random.seed)
+  }
+  # else {
+  #   rm(.Random.seed, envir=globalenv())
+  # }
+  
+  #' Shuffle cluster ids for each sample
+  se_metadata <- se.object@meta.data[, c(column.cluster.id, column.sample.id)]
+  se_metadata$clusters_original <- se_metadata[, column.cluster.id]
+  se_metadata$sample_id <- se_metadata[, column.sample.id]
+  
+  se_metadata_perm <- se_metadata %>% 
+    dplyr::group_by(sample_id) %>% 
+    dplyr::mutate(clusters_perm = seurat_clusters[sample(dplyr::row_number())])
+  
+  #' Add shuffled clusters to se object metadata
+  se.object <- AddMetaData(se.object, as.character(se_metadata_perm$clusters_perm), col.name = "clusters_perm")
+
+  return(se.object)
+}
+
+n_perm <- 50  # 10
+perm_adj_mat_list <- list()
+for(i in 1:n_perm){
+  se <- RandomiseClusteringIDs(se.object = se, column.cluster.id = "seurat_clusters", column.sample.id = "sample_id", random.seed = i)
+  se <- SetIdent(se, value = "clusters_perm")
+  for(column_rm in grep(pattern = "nbs_", colnames(se[[]]), value = T)){
+    se[[column_rm]] <- NULL
+  }
+  for(c in c_include){
+    se <- RegionNeighbours(se, id = c, keep.within.id = T, verbose = TRUE)
+  }
+  perm_nbs_df <- se[[]][, c("clusters_perm", grep(pattern = "nbs_", colnames(se[[]]), value = T))]
+  perm_adj_mat <- CreateAdjMatrix(nbs.df = perm_nbs_df, cluster.include = c_include, column.clusters.id = "clusters_perm")
+  perm_adj_mat_list[[i]] <- perm_adj_mat
+}
+
+n_cols <- dim(perm_adj_mat_list[[1]])[1]
+perm_adj_mat_avg <- perm_adj_mat_sd <- matrix(0L, nrow = n_cols, ncol = n_cols)
+for(i in 1:n_cols){
+  for(j in 1:n_cols){
+    list_ij <- c()
+    for(list_n in 1:length(perm_adj_mat_list)){
+      list_ij <- c(list_ij, perm_adj_mat_list[[list_n]][i,j])
+    }
+    perm_adj_mat_avg[i,j] <- mean(list_ij)
+    perm_adj_mat_sd[i,j] <- sd(list_ij)
+  }
+}
+
+
+#' Check if random data is normally dist.
+perm_adj_mat_df <- data.frame(matrix(unlist(perm_adj_mat_list), nrow=length(perm_adj_mat_list), byrow=T))  # dims: n_permX(18*18) = 50x324
+perm_adj_mat_df$perm_n <- rownames(perm_adj_mat_df)
+perm_adj_mat_df_long <- reshape2::melt(perm_adj_mat_df, id.vars = c("perm_n"))
+
+png(file.path(DIR_OUT, "../figures", paste0("nbs_analysis.permutation_distribution_check.", ANALYSIS, ".png")), width = 10*300, height = 6*300, res=300)
+ggplot(subset(perm_adj_mat_df_long, variable %in% paste0("X", 1:18)), aes(x=variable, y=value)) +
+  geom_boxplot() +
+  facet_wrap(~variable, scales = "free", ncol = 6)
+dev.off()
+
+#####
+#' Observed values: Run RegionNeighbours()
 se <- SetIdent(se, value = "seurat_clusters")
 for(column_rm in grep(pattern = "nbs", colnames(se[[]]), value = T)){
   se[[column_rm]] <- NULL
@@ -67,10 +198,9 @@ for(c in c_include){
 
 #' Create df with nbs info
 nbs_df <- se[[]][, c("seurat_clusters", grep(pattern = "nbs", colnames(se[[]]), value = T))]
+
 # write.table(nbs_df, file.path(DIR_RES, "tables", paste0("nbs_data.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T)
-
 # nbs_df <- read.table(file = file.path(DIR_RES, "tables", paste0("nbs_data.", ANALYSIS, ".tsv")), header = T, sep = "\t")
-
 
 c_count <- nbs_df %>%
   dplyr::group_by(seurat_clusters) %>%
@@ -84,24 +214,11 @@ c_nodes <- c_nodes[, c("id", "cluster", "cluster_anno", "cluster_group", "cluste
 
 
 #' Adjacency matrix
-nbs_adjmat <- matrix(0L, nrow = length(c_include), ncol = length(c_include))
-for (i in seq_along(c_include)) {
-  c <- c_include[i]
-  c_nbs_df <- nbs_df[nbs_df$seurat_clusters==c, ]
-  for (j in seq_along(c_include)) {
-    nbs <- paste0("nbs_", c_include[j])
-    if (j==i) {
-      n_nbs <- sum(!is.na(c_nbs_df[, nbs]==c))
-    } else {
-      n_nbs <- sum(!is.na(c_nbs_df[, nbs]==nbs))
-    }
-    nbs_adjmat[i,j] <- n_nbs
-    if (nbs_adjmat[j,i] > 0) {
-      nbs_adjmat[i,j] <- nbs_adjmat[j,i] <- max(c(nbs_adjmat[i,j], nbs_adjmat[j,i]))
-    }
-  }
-  nbs_adjmat[i,i] <- sum(nbs_adjmat[i,][-i])
-}
+nbs_adjmat <- CreateAdjMatrix(nbs.df = nbs_df, 
+                              cluster.include = c_include, 
+                              column.clusters.id = "seurat_clusters"
+                              )
+
 
 nbs_adjmat_norm <- nbs_adjmat
 for (i in seq(1:dim(nbs_adjmat_norm)[1])) {
@@ -114,14 +231,32 @@ for (i in seq(1:dim(nbs_adjmat_norm)[1])) {
 }
 
 
-rownames(nbs_adjmat) <- rownames(nbs_adjmat_norm) <- paste0("C", c_include)
-colnames(nbs_adjmat) <- colnames(nbs_adjmat_norm) <- paste0("C", c_include)
+#' "Normalize"/Standardise using permuted values
+nbs_adjmat_permscore <- round( ((nbs_adjmat - perm_adj_mat_avg) / perm_adj_mat_sd) , digits = 3)
+diag(nbs_adjmat_permscore) <- diag(nbs_adjmat)
 
+# nbs_adjmat_permscore2 <- round((nbs_adjmat_permscore / perm_adj_mat_sd), digits = 3 )
+# diag(nbs_adjmat_permscore2) <- diag(nbs_adjmat)
+
+#' Set row/column names
+rownames(nbs_adjmat) <- rownames(nbs_adjmat_norm) <- rownames(nbs_adjmat_permscore) <- paste0("C", c_include)
+colnames(nbs_adjmat) <- colnames(nbs_adjmat_norm) <- colnames(nbs_adjmat_permscore) <- paste0("C", c_include)
+
+rownames(perm_adj_mat_avg) <- rownames(perm_adj_mat_sd) <- colnames(perm_adj_mat_avg) <- colnames(perm_adj_mat_sd) <- paste0("C", c_include)
+
+#' Save tables
 # write.table(nbs_adjmat, file.path(DIR_RES, "tables", paste0("nbs_adj_matrix.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T, col.names = T)
 # write.table(nbs_adjmat_norm, file.path(DIR_RES, "tables", paste0("nbs_adj_matrix_norm.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T, col.names = T)
+# write.table(nbs_adjmat_permscore, file.path(DIR_RES, "tables", paste0("nbs_adj_matrix_permscore.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T, col.names = T)
+# 
+# write.table(perm_adj_mat_avg, file.path(DIR_RES, "tables", paste0("nbs_adj_matrix_perm_avg.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T, col.names = T)
+# write.table(perm_adj_mat_sd, file.path(DIR_RES, "tables", paste0("nbs_adj_matrix_perm_sd.", ANALYSIS, ".tsv")), quote = F, sep = "\t", row.names = T, col.names = T)
 
 
 # ===================================
+#' PLOTS
+color_low2 <- "#62376e"
+
 #' PLOT 1
 #' Make graph
 nbs_adjmat_df <- nbs_adjmat_norm
@@ -138,6 +273,10 @@ df <- data.frame(name = names(e_sum),
 links <- data.frame(
   source=as_edgelist(g, names = TRUE)[,1],
   target=as_edgelist(g, names = TRUE)[,2])
+
+g_df_norm <- data.frame(links,
+                        weight = E(g)$weight,
+                        weight_minmax = minmax_norm(abs(E(g)$weight)))
 
 g2 <- graph_from_data_frame(d = links, vertices = df, directed = F)
 
@@ -242,14 +381,13 @@ for(cluster_plot in names(e_sum)){
 dev.off()
 
 
-
-#' PLOT 2
+#' PLOT 2: cluster in center
 g3 <- g2
 E(g3)$width <- E(g2)$weight*4
 
 for(cluster_plot in c("C3", "C4", "C6")){
   e_pairs <- c()
-  for(c in names(e_sum)[!names(e_sum)==cluster_plot]){
+  for(c in names(e_sum)[!names(e_sum)==clusters_plot]){
     e_pairs <- c(e_pairs, cluster_plot, c)
   }
   ecol <- rep(NA, ecount(g3))
@@ -265,7 +403,9 @@ for(cluster_plot in c("C3", "C4", "C6")){
   cairo_ps(filename = file.path(DIR_FIG, paste0(fname, ".eps")), width = 5.5, height = 5.5)
   plot(g3, 
        layout=LS,
-       vertex.label.family = "Helvetica", vertex.label.color = "black", vertex.label.cex = 1.5,
+       vertex.label.family = "Helvetica", 
+       vertex.label.color = "black", 
+       vertex.label.cex = 1.5,
        vertex.size = V(g3)$norm_size*1.25,
        vertex.color = adjustcolor(V(g3)$color, alpha.f=1),
        vertex.frame.color="white",
@@ -273,5 +413,107 @@ for(cluster_plot in c("C3", "C4", "C6")){
        edge.curved=0)
   dev.off()
 }
+
+
+#' PLOT 3: Permuted score values
+#' Make graph
+nbs_adjmat_df <- nbs_adjmat_permscore
+g <- graph.adjacency(nbs_adjmat_df, mode = "undirected", weighted = TRUE, diag = F)
+
+e_sum <- diag(nbs_adjmat_df)
+df <- data.frame(name = names(e_sum),
+                 size = e_sum,
+                 norm_size = e_sum/max(e_sum)*40, 
+                 group = subset(c_nodes, id %in% names(e_sum))$cluster_group,
+                 color = subset(c_nodes, id %in% names(e_sum))$cluster_color,
+                 c_size = subset(c_nodes, id %in% names(e_sum))$n,
+                 stringsAsFactors = F)
+links <- data.frame(
+  source=as_edgelist(g, names = TRUE)[,1],
+  target=as_edgelist(g, names = TRUE)[,2])
+
+g_df_permscore <- data.frame(links,
+                             weight = E(g)$weight,
+                             weight_minmax = minmax_norm(abs(E(g)$weight)))
+
+
+g2 <- graph_from_data_frame(d = links, vertices = df, directed = F)
+
+g2 <- set_edge_attr(g2, "weight", value = minmax_norm(abs(E(g)$weight)))
+E(g2)$width <- (E(g2)$weight+0.1)*14
+
+
+fname <- paste0("nbs_analysis.permscore.", ANALYSIS)
+pdf(file = file.path(DIR_FIG, paste0(fname, ".pdf")), width = 5.5, height = 5.5)
+for(cluster_plot in names(e_sum)){
+  e_pairs <- c()
+  for(c in names(e_sum)[!names(e_sum)==cluster_plot]){
+    e_pairs <- c(e_pairs, cluster_plot, c)
+  }
+  e_weights <- E(g)$weight
+  ecol <- rep("grey90", ecount(g2))
+  ecol[get.edge.ids(g2, vp = e_pairs)] <- "grey20"
+  ecol[e_weights>0 & ecol=="grey20"] <- color_high  # viridis::viridis(10)[6]
+  ecol[e_weights<0 & ecol=="grey20"] <- color_low2  # viridis::viridis(10)[2]
+  ecol[ecol=="grey90"] <- NA
+  
+  node_ids <- names(V(g2))
+  node_e_widths <- E(g2)$width[get.edge.ids(g2, vp = e_pairs)]
+  node_sizes <- data.frame(node = node_ids,
+                           size = V(g2)$norm_size, 
+                           # width = c(node_e_widths[1:match(cluster_plot, node_ids)-1], 0, node_e_widths[match(cluster_plot, node_ids):length(node_e_widths)]),
+                           stringsAsFactors = F)
+  LS <- layout_as_star(g2, center = cluster_plot, order = order(node_sizes$size, decreasing = T))
+  plot(g2, 
+       layout=LS,
+       vertex.label.family = "Helvetica", vertex.label.color = "black", vertex.label.cex = 1.5,
+       vertex.size = V(g2)$norm_size,
+       vertex.color = adjustcolor(V(g2)$color, alpha.f=1),
+       vertex.frame.color="white",
+       edge.color = adjustcolor(ecol, alpha.f = .8),
+       edge.curved=0)
+}
+dev.off()
+
+
+#' PLOT HEATMAP
+library(pheatmap)
+library(scico)
+
+hm_df <- nbs_adjmat_permscore
+diag(hm_df) <- NA
+hm_df[lower.tri(hm_df)] <- NA
+
+hm_cluster_anno <- data.frame(row.names = paste0("C", c_anno$seurat_clusters),
+                              group = c_anno$cluster_group, stringsAsFactors = F)
+colors_group <- unique(c_anno$cluster_color)
+hm_colour = list(
+  group = c()
+)
+
+breaksList <- seq(-6, 6, by = 1)
+p1 <- pheatmap(hm_df,
+              cluster_rows = F, cluster_cols = F,
+              breaks = breaksList,
+              color = colorRampPalette(c(color_low2, "white", color_high))(length(breaksList)-1),
+              border_color = "white",
+              na_col = "white")
+p2 <- pheatmap(hm_df,
+              cluster_rows = F, cluster_cols = F,
+              breaks = breaksList,
+              color = (scico::scico(length(breaksList), palette = "vik")), #viridis::plasma(length(breaksList)),
+              border_color = "white",
+              na_col = "white")
+
+fname1 <- paste0("nbs_analysis.permscore_hm.", ANALYSIS)
+pdf(file = file.path(DIR_FIG, paste0(fname1, ".pdf")), width = 5.5, height = 5);p1;dev.off()
+
+fname2 <- paste0("nbs_analysis.permscore_hm2.", ANALYSIS)
+pdf(file = file.path(DIR_FIG, paste0(fname2, ".pdf")), width = 5.5, height = 5);p2;dev.off()
+
+
+#' Export tables
+write.csv(g_df_norm, file.path(DIR_RES, "tables", paste0("nbs_graph_df_norm.", ANALYSIS, ".csv")), quote = F, row.names = F)
+write.csv(g_df_permscore, file.path(DIR_RES, "tables", paste0("nbs_graph_df_permscore.", ANALYSIS, ".csv")), quote = F, row.names = F)
 
 
